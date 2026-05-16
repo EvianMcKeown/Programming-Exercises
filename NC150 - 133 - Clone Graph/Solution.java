45ab61c7580a3f13a18301b321bb1245575ff974
EvianMcKeown <evian.mck@gmail.com>
2026-05-16 11:32:11 +0200

    import existing files/folders

---
+++ NC150 - 133 - Clone Graph/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        //
+    }
+
+    public Node cloneGraph(Node node) {
+        return Node;
+    }
+
+    class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val, ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
+        }
+    }
+}
