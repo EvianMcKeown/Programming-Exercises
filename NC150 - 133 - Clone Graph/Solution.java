d8dc848454b045e136cb261d497ee665d23dcde5
EvianMcKeown <evian.mck@gmail.com>
2026-05-17 11:16:01 +0200

    add LC133 solution - beats 90% runtime, 87% memory

--- NC150 - 133 - Clone Graph/Solution.java
+++ NC150 - 133 - Clone Graph/Solution.java
@@ -1,13 +1,34 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 public class Solution {
-    public static void main(String[] args) {
-        //
-    }
+    // map val (id) → object, can thus track if we kave seen a Node before
+    private Map<Integer,Node> seenMap = new HashMap<>();
 
     public Node cloneGraph(Node node) {
-        return Node;
+
+        // base case
+        if (node == null) {
+            return null;
+        }
+
+        // if Node seen before, return clone
+        if (seenMap.containsKey(node.val)) {
+            return seenMap.get(node.val);
+        }
+
+        // clone node with empty adjacency list, and store it before traversal
+        Node clonedNode = new Node(node.val);
+        seenMap.put(node.val, clonedNode);
+
+        // iterate through original Node's neighbors
+        for (int i=0; i < node.neighbors.size(); i++) {
+            clonedNode.neighbors.add(cloneGraph(node.neighbors.get(i)));
+        }
+
+        return clonedNode;
     }
 
     class Node {
